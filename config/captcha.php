ba0d9425207a87220af90dac08e5026b5e54fbd9
Sazidur Rahman Sadi <sazidurrahmansadi@gmail.com>
2021-07-16 21:53:02 +0600

    Update captcha.php

--- config/captcha.php
+++ config/captcha.php
@@ -7,7 +7,7 @@ return [
         'width' => 120,
         'height' => 36,
         'quality' => 100,
-        'math' => false,
+        'math' => true,
         'expire' => 10000,
         'encrypt' => false,
     ],
